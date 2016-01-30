d65242ff23d265a174f0be19096d2b840496b86b
Ivan Wang <icw@cmu.edu>
2016-01-30 13:10:45 -0500

    added boulder minimap

---
+++ Assets/Scripts/UI/Map.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+    public float maxHeight = 500f;
+    public float maxDepth = 500f;
+    public Transform boulder;
+
+    public RectTransform boulderTransform;
+    public RectTransform highestTransform;
+
+    void Start () {
+    }
+
+    void Update () {
+        Vector2 newPos = boulderTransform.localPosition;
+        newPos.y = Mathf.Clamp(0f, (boulder.position.z / maxDepth) * maxHeight, maxHeight);
+        boulderTransform.localPosition = newPos;
+
+        if (newPos.y > highestTransform.localPosition.y) {
+            highestTransform.localPosition = newPos;
+        }
+    }
+
+}
