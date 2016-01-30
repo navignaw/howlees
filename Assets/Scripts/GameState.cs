b4ec1481064eec29af50f43dd6f17841a7cb5a43
Ralph <ralph@puppyrush.net>
2016-01-29 23:29:20 -0500

    Checking in for the night

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -2,14 +2,93 @@
 using System.Collections;
 
 public class GameState : MonoBehaviour {
+	const int NIGHT = 0;
+	const int DAY = 1;
+	const int UPGRADE = 2;
+	const int PAUSE = 3;
+	const int INTRO = 4;
+	const int START = 5;
+	int curGameState;
+	Boulder boulder;
+	Sisyphus sisyphus;
+	GameObject pauseScreen;
+	GameObject upgradeScreen;
+	GameObject startScreen;
 
 	// Use this for initialization
 	void Start () {
-	
+		TurnIntro();
+		sisyphus = GameObject.Find("Sisyphus").GetComponent<Sisyphus>();
+		boulder = GameObject.Find("Boulder").GetComponent<Boulder>();
+		pauseScreen = GameObject.Find("PauseScreen");
+		upgradeScreen = GameObject.Find("UpgradeScreen");
+		startScreen = GameObject.Find("StartScreen");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () {		
+		switch (curGameState)
+		{
+			case NIGHT:
+				break;
+			case DAY:
+				break;
+			case UPGRADE:
+				break;
+			case PAUSE:
+				break;
+			case INTRO:
+				break;
+		}
+	}
+
+	void TurnNight ()
+	{
+		Init();
+		curGameState = NIGHT;
+	}	
+
+	void TurnDay ()
+	{
+		Init();
+		curGameState = DAY;
+		sisyphus.SetPlayable(true);
+		boulder.SetPlayable(true);
+	}
+
+	void TurnUpgrade ()
+	{
+		Init();
+		curGameState = UPGRADE;
+		upgradeScreen.SetActive(true);
+	}
+
+	public void TurnPause ()
+	{
+		Init();
+		curGameState = PAUSE;
+	}	
+
+	public void TurnIntro ()
+	{
+		Init();
+		curGameState = INTRO;
+		Camera.main.GetComponent<CameraMove>().RunIntro();
+	}
+
+	public void TurnStart ()
+	{
+		Init();
+		curGameState = START;
+		startScreen.SetActive(true);
+	}
+
+	void Init()
+	{
+		sisyphus.SetPlayable(false);
+		boulder.SetPlayable(false);
+		upgradeScreen.SetActive(false);
+		pauseScreen.SetActive(false);
+		startScreen.SetActive(false);
 	}
 }
