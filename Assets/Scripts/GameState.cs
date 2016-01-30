86f75e51626f7d37682053ae96795ae7f07b0658
Ralph <ralph@puppyrush.net>
2016-01-30 14:39:43 -0500

    time passage and sun works

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -16,18 +16,16 @@ public class GameState : MonoBehaviour {
 	public static int day = 0;
 	public static float bestDistance = 0f;
 	public static float time = 0f;
+	public static float morning = 90;
+	public static float noon = 180;
+	public static float night = 270;
 
 	static float dayTimeScale = 10;
 	static float nightTimeScale = 100;
 	static float timeScale = 10;
 
-	static Color morningSun = new Color(127, 255, 230);
-	static Color noonSun = new Color(254, 134, 112);
-	static Color nightSun = new Color(101, 91, 194);
-	static Color sunColor;
-
 	public Sisyphus sisyphus;
-	public GameObject sun;
+	public Sun sun;
 	public GameObject pauseScreen;
 	public GameObject upgradeScreen;
 	public GameObject startScreen;
@@ -45,24 +43,21 @@ public class GameState : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		time = time % 360;
-		sun.transform.eulerAngles = new Vector3 (0,180,time);
-		sunColor = ColorMap(sunColor, 0, 90, morningSun, noonSun);
-		print(sunColor);
-		sun.GetComponentInChildren<SpriteRenderer>().color = sunColor;
-
+		sun.Tick(time);
 
 		switch (curGameState)
 		{
 			case State.NIGHT:
 			{
 				/* speed up to sundown */
-				if (time < 270)	time += timeScale * Time.deltaTime;
+				if (time < morning || time >= night) 
+					time += nightTimeScale * Time.deltaTime;
 			}
 				break;
 			case State.DAY:
 			{
 				time += timeScale * Time.deltaTime;
-				if (time >= 180) TurnNight();
+				if (time >= night) TurnNight();
 			}
 				break;
 			case State.UPGRADE:
@@ -78,7 +73,6 @@ public class GameState : MonoBehaviour {
 	{
 		Init();
 		curGameState = State.NIGHT;
-		timeScale = nightTimeScale;
 	}
 
 	static public void TurnDay ()
@@ -87,6 +81,7 @@ public class GameState : MonoBehaviour {
 		GameState.day++;
 		curGameState = State.DAY;
 		timeScale = dayTimeScale;
+		time = morning;
 		gameState.gameScreen.SetActive(true);
 		gameState.sisyphus.SetPlayable(true);
 		//boulder.SetPlayable(true);
@@ -127,13 +122,5 @@ public class GameState : MonoBehaviour {
 		gameState.pauseScreen.SetActive(false);
 		gameState.startScreen.SetActive(false);
 		gameState.gameScreen.SetActive(false);
-	}
-
-	Color ColorMap (Color value, float fromSource, float toSource, Color fromTarget, Color toTarget)
-	{
-		value.r = (value.r - fromSource) / (toSource - fromSource) * (toTarget.r - fromTarget.r);
-		value.g = (value.g - fromSource) / (toSource - fromSource) * (toTarget.g - fromTarget.g);
-		value.b = (value.b - fromSource) / (toSource - fromSource) * (toTarget.b - fromTarget.b);
-		return value;
 	}
 }
---
+++ Assets/Scripts/Sun.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sun : MonoBehaviour {
+	static Color morningSun = new Color(0.498f, 1, 0.902f);
+	static Color noonSun = new Color(1, 0.525f, 0.439f);
+	static Color nightSun = new Color(0.396f, 0.357f, 0.760f);
+	static Color sunColor = new Color(0,0,0);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void Tick (float time)
+	{
+		this.transform.eulerAngles = new Vector3 (0,180,time);
+		sunColor = ColorMap(time);
+		this.GetComponentInChildren<SpriteRenderer>().color = sunColor;
+		print(sunColor);
+	}
+
+	Color ColorMap (float value)
+	{
+		Color newColor = new Color(0,0,0);
+		float fromSource = 90;
+		float toSource = 270;
+		Color fromTarget = morningSun;
+		Color toTarget = nightSun;
+		if (value > GameState.morning && value <= GameState.noon)
+		{	
+			fromSource = GameState.morning;
+			toSource = GameState.noon;
+			fromTarget = morningSun;
+			toTarget = noonSun;
+		}
+		if (value > GameState.noon && value <= GameState.night)
+		{
+			fromSource = GameState.noon;
+			toSource = GameState.night;
+			fromTarget = noonSun;
+			toTarget = nightSun;
+		}
+		newColor.r = (value - fromSource) / (toSource - fromSource) * (toTarget.r - fromTarget.r) + fromTarget.r;
+		newColor.g = (value - fromSource) / (toSource - fromSource) * (toTarget.g - fromTarget.g) + fromTarget.g;
+		newColor.b = (value - fromSource) / (toSource - fromSource) * (toTarget.b - fromTarget.b) + fromTarget.b;
+		return newColor;
+	}
+}
