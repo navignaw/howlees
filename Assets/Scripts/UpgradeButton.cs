9ee1c1cb31fee1af17d38d8a53b7a7ad88dfbca5
Ivan Wang <icw@cmu.edu>
2016-01-30 20:05:21 -0500

    update upgrades with costs

---
+++ Assets/Scripts/UpgradeButton.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UpgradeButton : MonoBehaviour {
+    public enum ButtonType {
+        AESTHETIC,
+        STRENGTH,
+        STAMINA,
+        TRACTION
+    }
+
+    public Equation costEquation;
+    public ButtonType type;
+
+    private Button button;
+    private int cost;
+
+    // Use this for initialization
+    void Start () {
+        button = GetComponent<Button>();
+        UpdateCost();
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+
+    public void UpdateCost() {
+        switch (type) {
+            case ButtonType.AESTHETIC:
+                cost = (int) costEquation.eval(Upgrades.aesthetics);
+                break;
+
+            case ButtonType.STRENGTH:
+                cost = (int) costEquation.eval(Upgrades.strengthUpgrade);
+                break;
+
+            case ButtonType.STAMINA:
+                cost = (int) costEquation.eval(Upgrades.staminaUpgrade);
+                break;
+
+            case ButtonType.TRACTION:
+                cost = (int) costEquation.eval(Upgrades.tractionUpgrade);
+                break;
+        }
+        button.interactable = GameState.karma > cost;
+        // TODO: update text
+    }
+
+}
--- Assets/Scripts/Upgrades.cs
+++ Assets/Scripts/Upgrades.cs
@@ -4,9 +4,13 @@ using System.Collections;
 public class Upgrades : MonoBehaviour {
     public Sisyphus sisyphus;
     public Equation strengthFormula; // how fast strength grows
+    public Equation staminaFormula;  // how fast stamina grows
+    public Equation tractionFormula; // how fast traction grows
 
-    public static int strengthUpgrade = 0;
     public static int aesthetics = 0;
+    public static int strengthUpgrade = 0;
+    public static int staminaUpgrade = 0;
+    public static int tractionUpgrade = 0;
 
 
     // Use this for initialization
@@ -21,7 +25,34 @@ public class Upgrades : MonoBehaviour {
 
     public void UpdateUpgrades() {
         sisyphus.maxStrength = strengthFormula.eval(strengthUpgrade);
+        sisyphus.energyDepleteRate = staminaFormula.eval(staminaUpgrade);
+        //sisyphus.energyDepleteRate = staminaFormula.eval(staminaUpgrade);
+        // TODO: traction
+
+        foreach (UpgradeButton button in GetComponentsInChildren<UpgradeButton>()) {
+            button.UpdateCost();
+        }
     }
 
+    public void BuyAestheticUpgrade() {
+        aesthetics++;
+        // TODO: stuff
+        UpdateUpgrades();
+    }
+
+    public void BuyStrengthUpgrade() {
+        strengthUpgrade++;
+        UpdateUpgrades();
+    }
+
+    public void BuyStaminaUpgrade() {
+        staminaUpgrade++;
+        UpdateUpgrades();
+    }
+
+    public void BuyTractionUpgrade() {
+        tractionUpgrade++;
+        UpdateUpgrades();
+    }
 
 }
