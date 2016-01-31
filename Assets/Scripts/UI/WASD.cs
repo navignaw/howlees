d0c28045e69605eb13e4681754f1d75173f241f2
Ivan Wang <icw@cmu.edu>
2016-01-31 05:22:13 -0500

    mist and wasd instructions

---
+++ Assets/Scripts/UI/WASD.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WASD : MonoBehaviour {
+    public float animateSpeed = 0.5f;
+
+    private SpriteRenderer spriteRenderer;
+    private bool fading = false;
+
+    // Use this for initialization
+    void Awake () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!fading && Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
+            fading = true;
+        }
+
+        // fade opacity
+        if (fading) {
+            Color newColor = spriteRenderer.color;
+            newColor.a = Mathf.Clamp01(newColor.a - animateSpeed * Time.deltaTime);
+            spriteRenderer.color = newColor;
+
+            if (newColor.a <= 0.01f) {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
